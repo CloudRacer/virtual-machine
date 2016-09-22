b7ee122f6724f0087d290788748d8d48b4cdb240
John McDonnell <jmcdonnell3@csc.com>
2016-09-22 05:13:51 +0100

    Backup script.

---
+++ software/backup.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+setScriptFilename() {
+	SCRIPT_FILE=`basename $0`
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		echo "ERR: $RESULT: Error encountered while determining the name of the current script."
+		return $RESULT;
+	fi
+
+	return 0
+}
+
+setScriptFolderName() {
+	SCRIPT_FOLDER=`dirname $0`;
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		echo "ERR: $RESULT: Error encountered while determining the name of the folder containing the current script."
+		return $RESULT;
+	fi
+	
+	if [ "$SCRIPT_FOLDER" = "" ] || [ "$SCRIPT_FOLDER" = "." ] || [ -z "$SCRIPT_FOLDER" ]; then
+		SCRIPT_FOLDER=`pwd`
+	fi
+
+	return 0
+}
+
+initialiseEnvironmentVariables() {
+    if [ ! -z "$ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME" ]; then
+        if [ -f $ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME ]; then
+            . $ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME
+            RESULT=$?
+            if [ $RESULT -ne 0 ]; then
+                return $RESULT
+            fi
+        fi
+    fi
+
+	return 0
+}
+
+initialiseEnvironment() {
+    initialiseEnvironmentVariables
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+        return $RESULT
+    fi
+	
+	setScriptFilename
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		return $RESULT
+	fi
+	
+	setScriptFolderName
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		return $RESULT
+	fi
+	
+	return 0
+}
+
+finalise() {
+    initialiseEnvironmentVariables
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+        return $RESULT
+    fi
+
+    return 0
+}
+
+main() {
+	initialiseEnvironment
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		return $RESULT
+	fi
+	
+	return 0
+}
+
+main
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+	return $RESULT
+fi
+
+FOLDER_NAME=$1
+BACKUP_FOLDER=~/.backup
+ARCHIVE_FILENAME=$BACKUP_FOLDER/`basename $FOLDER_NAME`-`date "+%Y-%m-%d_%H-%M-%S"`.tar.gz
+UTILITY_FETCH_FOLDER=$SCRIPT_FOLDER/create-folder.sh
+ARCHIVE_UTILITY="tar -cvpzf"
+
+echo FOLDER_NAME:$FOLDER_NAME.
+echo BACKUP_FOLDER:$BACKUP_FOLDER.
+echo ARCHIVE_FILENAME:$ARCHIVE_FILENAME.
+echo UTILITY_FETCH_FOLDER:$UTILITY_FETCH_FOLDER.
+echo ARCHIVE_UTILITY:$ARCHIVE_UTILITY.
+
+"$UTILITY_FETCH_FOLDER" "$BACKUP_FOLDER"
+
+$ARCHIVE_UTILITY "$ARCHIVE_FILENAME" "$FOLDER_NAME"
+
+finalise
