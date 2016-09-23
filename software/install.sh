98fdeb486b183e1242bd73feb29bdbbc06a4ee94
John McDonnell <v7mcdoj@swisslog.com>
2016-09-23 16:30:17 +0100

    batch install.

---
+++ software/install.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+setScriptFilename() {
+	SCRIPT_FILE=`basename $0`
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		echo "ERR: $RESULT: Error encountered while determining the name of the current script."
+		return $RESULT;
+	fi
+
+	return 0
+}
+
+setScriptFolderName() {
+	SCRIPT_FOLDER=`dirname $0`;
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		echo "ERR: $RESULT: Error encountered while determining the name of the folder containing the current script."
+		return $RESULT;
+	fi
+	
+	if [ "$SCRIPT_FOLDER" = "" ] || [ "$SCRIPT_FOLDER" = "." ] || [ -z "$SCRIPT_FOLDER" ]; then
+		SCRIPT_FOLDER=`pwd`
+	fi
+
+	return 0
+}
+
+initialiseEnvironmentVariables() {
+    if [ ! -z "$ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME" ]; then
+        if [ -f $ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME ]; then
+            . $ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME
+            RESULT=$?
+            if [ $RESULT -ne 0 ]; then
+                return $RESULT
+            fi
+        fi
+    fi
+
+	return 0
+}
+
+initialiseEnvironment() {
+    initialiseEnvironmentVariables
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+        return $RESULT
+    fi
+	
+	setScriptFilename
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		return $RESULT
+	fi
+	
+	setScriptFolderName
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		return $RESULT
+	fi
+	
+	return 0
+}
+
+finalise() {
+    initialiseEnvironmentVariables
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+        return $RESULT
+    fi
+
+    return 0
+}
+
+main() {
+	initialiseEnvironment
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		return $RESULT
+	fi
+	
+	return 0
+}
+
+main
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+	return $RESULT
+fi
+
+INSTALL_LIST=$SCRIPT_FOLDER/`echo install.list | cut -f 1 -d '.'`.list
+UTILITY_INSTALL_APPLICATION_FOLDER=$SCRIPT_FOLDER/install-application.sh
+ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME=/etc/environment
+
+echo INSTALL_LIST:$INSTALL_LIST.
+echo UTILITY_INSTALL_APPLICATION_FOLDER:$UTILITY_INSTALL_APPLICATION_FOLDER.
+echo ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME:$ENVIRONMENT_VALIABLE_SYSTEM_WIDE_FILENAME.
+
+echo
+echo
+echo
+
+cat "$INSTALL_LIST"
+
+echo
+echo
+echo
+
+IFS=","
+while read NAME URL EXE
+do
+    "$UTILITY_INSTALL_APPLICATION_FOLDER" "$NAME" "$URL" "$EXE"
+done < "$INSTALL_LIST"
+
+finalise
