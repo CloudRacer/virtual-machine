688c4e24a227f23ca2db6e5395d015c7fe487d30
John McDonnell <jmcdonnell3@csc.com>
2016-09-26 21:14:56 +0100

    curl replaces wget.

---
+++ server/test/version.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+. /etc/environment
+
+echo
+echo
+echo
+echo "***** Linux *****"
+uname -a
+lsb_release -a
+echo "******************"
+
+echo
+echo
+echo
+echo "***** Java *****"
+java -version
+echo "******************"
+
+echo
+echo
+echo
+echo "***** Maven *****"
+mvn --version
+echo "******************"
+
+echo
+echo
+echo
+echo "***** Git *****"
+git --version
+echo "******************"
+
+echo
+echo
+echo
+echo "***** npm *****"
+npm -v
+echo "******************"
+
+echo
+echo
+echo
+echo "***** Bower *****"
+bower -v
+echo "******************"
+
+echo
+echo
+echo
+echo "***** Vagrant *****"
+vagrant --version 
+echo "******************"
